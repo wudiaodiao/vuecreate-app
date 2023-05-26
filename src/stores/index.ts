287f06131c41f45592e75748636b99733d6dd28f
wudiaodiao <1325789671@qq.com>
2023-05-26 18:12:08 +0800

    5/26

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -209,5 +209,146 @@ export const useAuthStore = defineStore('AuthStore', {
       state.user = val
     },
   },
-  modules: {}
+  charAt: function (pos: number): string {
+    throw new Error('Function not implemented.')
+  },
+  charCodeAt: function (index: number): number {
+    throw new Error('Function not implemented.')
+  },
+  concat: function (...strings: string[]): string {
+    throw new Error('Function not implemented.')
+  },
+  indexOf: function (searchString: string, position?: number | undefined): number {
+    throw new Error('Function not implemented.')
+  },
+  lastIndexOf: function (searchString: string, position?: number | undefined): number {
+    throw new Error('Function not implemented.')
+  },
+  localeCompare: function (that: string): number {
+    throw new Error('Function not implemented.')
+  },
+  match: function (regexp: string | RegExp): RegExpMatchArray | null {
+    throw new Error('Function not implemented.')
+  },
+  replace: function (searchValue: string | RegExp, replaceValue: string): string {
+    throw new Error('Function not implemented.')
+  },
+  search: function (regexp: string | RegExp): number {
+    throw new Error('Function not implemented.')
+  },
+  slice: function (start?: number | undefined, end?: number | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  split: function (separator: string | RegExp, limit?: number | undefined): string[] {
+    throw new Error('Function not implemented.')
+  },
+  substring: function (start: number, end?: number | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  toLowerCase: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  toLocaleLowerCase: function (locales?: string | string[] | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  toUpperCase: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  toLocaleUpperCase: function (locales?: string | string[] | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  trim: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  length: 0,
+  substr: function (from: number, length?: number | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  codePointAt: function (pos: number): number | undefined {
+    throw new Error('Function not implemented.')
+  },
+  includes: function (searchString: string, position?: number | undefined): boolean {
+    throw new Error('Function not implemented.')
+  },
+  endsWith: function (searchString: string, endPosition?: number | undefined): boolean {
+    throw new Error('Function not implemented.')
+  },
+  normalize: function (form: 'NFC' | 'NFD' | 'NFKC' | 'NFKD'): string {
+    throw new Error('Function not implemented.')
+  },
+  repeat: function (count: number): string {
+    throw new Error('Function not implemented.')
+  },
+  startsWith: function (searchString: string, position?: number | undefined): boolean {
+    throw new Error('Function not implemented.')
+  },
+  anchor: function (name: string): string {
+    throw new Error('Function not implemented.')
+  },
+  big: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  blink: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  bold: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  fixed: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  fontcolor: function (color: string): string {
+    throw new Error('Function not implemented.')
+  },
+  fontsize: function (size: number): string {
+    throw new Error('Function not implemented.')
+  },
+  italics: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  link: function (url: string): string {
+    throw new Error('Function not implemented.')
+  },
+  small: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  strike: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  sub: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  sup: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  padStart: function (maxLength: number, fillString?: string | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  padEnd: function (maxLength: number, fillString?: string | undefined): string {
+    throw new Error('Function not implemented.')
+  },
+  trimEnd: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  trimStart: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  trimLeft: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  trimRight: function (): string {
+    throw new Error('Function not implemented.')
+  },
+  matchAll: function (regexp: RegExp): IterableIterator<RegExpMatchArray> {
+    throw new Error('Function not implemented.')
+  },
+  replaceAll: function (searchValue: string | RegExp, replaceValue: string): string {
+    throw new Error('Function not implemented.')
+  },
+  at: function (index: number): string | undefined {
+    throw new Error('Function not implemented.')
+  },
+  [Symbol.iterator]: function (): IterableIterator<string> {
+    throw new Error('Function not implemented.')
+  }
 })
